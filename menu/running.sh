33f021751cae556432ae9ff4c1da9ad5251dce11
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-12 16:21:08 +0700

    Update running.sh

--- menu/running.sh
+++ menu/running.sh
@@ -245,8 +245,8 @@ kernelku=$(uname -r)
 
 # DNS PATCH
 #tipeos2=$(uname -m)
-Name=$(curl -sS https://raw.githubusercontent.com/kamunikan/permission/main/ipmini | grep $MYIP | awk '{print $2}')
-Exp=$(curl -sS https://raw.githubusercontent.com/kamunikan/permission/main/ipmini | grep $MYIP | awk '{print $3}')
+Name=$(curl -sS https://raw.githubusercontent.com/sibeesans/permission/main/ipmini | grep $MYIP | awk '{print $2}')
+Exp=$(curl -sS https://raw.githubusercontent.com/sibeesans/permission/main/ipmini | grep $MYIP | awk '{print $3}')
 # GETTING DOMAIN NAME
 Domen="$(cat /etc/xray/domain)"
 echo -e ""
