7b6f4f36d347e4f69eb25c24e74ed324f0fa068d
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-13 17:49:39 +0700

    Update del-tr.sh

--- xray/del-tr.sh
+++ xray/del-tr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SL
+# My Telegram : https://t.me/Admmoi
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -12,16 +12,10 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl ipinfo.io/ip | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Fuck You!!"
-exit 0
-fi
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
@@ -57,4 +51,4 @@ echo "================================"
 echo "Username  : $user"
 echo "Expired   : $exp"
 echo "================================"
-echo "Script Mod By ADAM SIJA"
+echo "Script By MIGtunnel"
