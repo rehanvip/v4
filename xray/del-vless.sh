3edb0db83a2d6cf4df4aae0ca72e6691782513ab
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-13 22:58:37 +0700

    Update del-vless.sh

--- xray/del-vless.sh
+++ xray/del-vless.sh
@@ -48,8 +48,4 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#& " "/etc/xray/config.json")
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo ""
     read -n 1 -s -r -p "Press any key to back on menu"
-    
-    v2ray-menu
-    fi
-
 
